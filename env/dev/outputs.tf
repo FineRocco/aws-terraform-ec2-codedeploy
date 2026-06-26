9ead0f6b1a983cbb33367fcdd2f1193ea2de7d4b
Denis Ungureanu <l3ti.me20@gmail.com>
2026-06-26 23:51:43 +0100

    Update output configurations in Terraform for dev and prod environments

--- env/dev/outputs.tf
+++ env/dev/outputs.tf
@@ -1,14 +1,14 @@
 output "ecr_repository_url" {
   description = "The ECR URL for the GitHub Actions pipeline"
-  value       = module.prod_stack.ecr_repository_url
+  value       = module.dev_stack.ecr_repository_url
 }
 
-output "prod_db_endpoint" {
-  description = "The RDS connection string for Prod"
-  value       = module.prod_stack.db_endpoint
+output "dev_db_endpoint" {
+  description = "The RDS connection string"
+  value       = module.dev_stack.db_endpoint
 }
 
-output "prod_web_public_ip" {
-  description = "The public IP of the Prod web server"
-  value       = module.prod_stack.web_public_ip
+output "dev_web_public_ip" {
+  description = "The public IP of the Dev web server"
+  value       = module.dev_stack.web_public_ip
 }
