b9332c9a8abe1b4939158f48e8d5b1de13a51b73
Denis Ungureanu <l3ti.me20@gmail.com>
2026-06-27 00:16:28 +0100

    Remove commented lines and sensitive database password variable from Dockerfile and Terraform variables

--- env/dev/variables.tf
+++ env/dev/variables.tf
@@ -7,9 +7,3 @@ variable "instance_type" {
   description = "EC2 instance size"
   type        = string
 }
-
-variable "db_password" {
-  description = "The password for the RDS PostgreSQL database"
-  type        = string
-  sensitive   = true
-}
